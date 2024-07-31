ade4cbea840b06fe81adaea77d59a85773d886a6
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-07-31 11:45:18 +0800

    Create password complexity for stricter validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  validate :password_complexity
+
+  def password_complexity
+    #regex does not include password length.
+    #Length requirement is handled by config.password.length at config/initializers/devise.rb
+    return if password.blank? || password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+
+    errors.add :password, "Your password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character."
+  end
 end
