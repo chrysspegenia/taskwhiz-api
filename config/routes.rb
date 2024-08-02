f73413fc3419c436063741ad6c4223e9b9f07524
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-08-02 17:50:01 +0800

    Add user controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,13 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
+  resources :users, only: [:show, :update] do
+    collection do
+      patch "update_password"
+    end
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
