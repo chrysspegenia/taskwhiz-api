f0815837e7464cdd91fe3d35788fc05e22843b5c
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-08-05 17:52:59 +0800

    Create routes for boards_controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,11 @@ Rails.application.routes.draw do
     end
   end
   
+  namespace :api do
+    namespace :v1 do
+      resources :boards
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
