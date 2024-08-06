54b7e3dd79a57e61567066c40aac5f1f05b30b5b
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-08-06 17:52:49 +0800

    Setup mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,5 +69,16 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.example.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            Rails.application.credentials.gmail[:username],
+    password:             Rails.application.credentials.gmail[:password],
+    authentication:       'plain',
+    enable_starttls_auto: true
+}
+
+config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -87,4 +87,19 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  #Include deployed link in host once deployed
+  config.action_mailer.default_url_options = { host: '', protocol: 'https' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.example.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            Rails.application.credentials.gmail[:username],
+    password:             Rails.application.credentials.gmail[:password],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
 end
