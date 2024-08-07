2d0fb37cc786008b456a91afd3c2560cc3ba3188
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-08-07 18:14:47 +0800

    Include welcome mailer when successful registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,6 +10,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
         status: {code: 200, message: 'Signed up successfully.'},
         data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
       }
+      # UserMailer.with(user: current_user).welcome_email.deliver_now
+      UserMailer.welcome_email(user: current_user).deliver_later
     else
       render json: {
         status: {message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}"}
