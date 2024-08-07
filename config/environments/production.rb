306da8349395f892906cddc44e8e88f2aa5bf176
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-08-07 18:14:08 +0800

    Modify smtp address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.example.com',
+    address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
     user_name:            Rails.application.credentials.gmail[:username],
