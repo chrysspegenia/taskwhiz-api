a61f4e9cdeef2f201df068104122d6dfccc39140
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-08-07 18:03:10 +0800

    Add devise password parameter sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::API
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name avatar])
+    devise_parameter_sanitizer.permit(:password, keys: [:email])
   end
 end
